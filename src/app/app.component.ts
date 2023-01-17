1df693149919ad97e3f065c243fccdd6a413f10b
Arya Putra Bayu Nanda <ochaarya5@gmail.com>
2023-01-17 20:47:51 +0700

    UASPMW

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'UAS202102341';
+  NIM = '202102341';
+  Nama = 'Arya Putra B. N';
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MahasiswaComponent } from './mahasiswa/mahasiswa.component';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MahasiswaComponent,
+    HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
